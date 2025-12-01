0649ac9a61e7dd62fb77c7450e3832704fdf4aba
mg <mathieu.george92@gmail.com>
2025-12-01 07:49:22 +0100

    Day1

--- src/main/java/day1/Day1.java
+++ src/main/java/day1/Day1.java
@@ -1,5 +1,21 @@
 void main() throws IOException {
     Path inputPath = Paths.get("src/main/java/day1/input.txt");
     List<String> lines = Files.readAllLines(inputPath);
-    IO.println(lines);
+
+    int pos = 50;
+    int count = 0;
+    for (String line : lines) {
+        boolean isLeft = line.charAt(0) == 'L';
+        int value = Integer.parseInt(line.substring(1));
+        //pos -= value;
+        for (int i = 0; i < value; i++) {
+            pos = isLeft ? pos - 1 : pos + 1;
+            pos %= 100;
+            if (pos == 0) count++;
+        }
+        // pos += value;
+        pos %= 100;
+    }
+    IO.println(count);
+    // 6671
 }
