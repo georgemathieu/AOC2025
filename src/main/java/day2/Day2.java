a98195db0e576fa82a31be9dc7b0486b540757f8
mathieu.george@kshuttle.io <mathieu.george@kshuttle.io>
2025-12-02 15:22:04 +0100

    Day 2

--- src/main/java/day2/Day2.java
+++ src/main/java/day2/Day2.java
@@ -1,37 +1,42 @@
 void main() throws IOException {
     Path inputPath = Paths.get("src/main/java/day2/input.txt");
     List<String> lines = Files.readAllLines(inputPath);
-
     String[] ranges = lines.getFirst().split(",");
-
-    long count = 0;
+    long sum = 0;
     for (String range : ranges) {
-
         String[] content = range.split("-");
         String startCode = content[0];
-        long startValue = Long.parseLong(startCode);
-        String startTruncatedCode = startCode.substring(0, startCode.length() / 2);
-        int startTruncatedValue = Integer.parseInt(startTruncatedCode);
-
         String endCode = content[1];
+        long startValue = Long.parseLong(startCode);
         long endValue = Long.parseLong(endCode);
-        String endTruncatedCode = endCode.substring(0, endCode.length() / 2);
-        int endTruncatedValue = Integer.parseInt(endTruncatedCode);
-
-        for (int i = startTruncatedValue; i < endTruncatedValue; i++) {
-
+        for (long i = startValue; i <= endValue; i++) {
+            String code = Long.toString(i);
+            StringBuilder acc = new StringBuilder();
+            int size = 0;
+            for (char c : code.toCharArray()) {
+                if (size >= code.length() / 2) break;
+                acc.append(c);
+                String regex = "^(" + acc +  ")+$";
+                Matcher m = Pattern.compile(regex).matcher(code);
+                if (m.find()) {
+                    sum += i;
+                    break;
+                }
+                size ++;
+            }
+            /*if (!isValid("" + i)) {
+                sum += i;
+            }*/
         }
     }
-
-
+    IO.println(sum);
+    // P1 54234399924
+    // P2 70187097315
 }
-
-record Range(int start, String startCode, int end, String endCode) { }
 
 boolean isValid(String code) {
     if (code.length() % 2 != 0) {
         return true;
     }
-
     return !code.substring(0, code.length() / 2).equals(code.substring(code.length() / 2));
 }
