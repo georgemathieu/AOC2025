d865a836e535c5e1873b483a4bf2f9b9c095f5dd
mg <mathieu.george92@gmail.com>
2025-12-05 19:22:51 +0100

    P2 Day5

--- src/main/java/day5/Day5.java
+++ src/main/java/day5/Day5.java
@@ -27,23 +27,17 @@ void main() throws IOException {
     // Merge ranges into sequences
     Collections.sort(ranges);
     for (int i = 0; i < ranges.size(); i++) {
-        Range r = ranges.get(i);
+        Range current = ranges.get(i);
         if (i + 1 == ranges.size()){
             break;
         }
         Range next = ranges.get(i + 1);
-        if (next.start <= r.end) {
-            ranges.set(i, new Range(r.start, next.end));
+        if (next.start <= current.end + 1) {
+            ranges.set(i, new Range(current.start, Math.max(current.end, next.end)));
             ranges.remove(i + 1);
             i--;
         }
-
-        /*if (r.contains(next.start) && r.contains(next.end)) {
-            ranges.remove(i + 1);
-            i--;
-        }*/
     }
-
 
     long countTotal = 0;
     for (Range range : ranges) {
@@ -53,13 +47,7 @@ void main() throws IOException {
     IO.println("P1 : " + count);
     IO.println("Total : " + countTotal);
     // P1 598
-    // naze 372718792674544
-    // naze 386870782265492
-    // naze 185771434702083
-    // naze 285363856093303
-    // naze 284751762953323
-    // naze 198247628899022
-    // naze 307844120396308
+    // P2 360341832208407
 }
 
 private record Range(long start, long end) implements Comparable<Range> {
@@ -69,11 +57,6 @@ private record Range(long start, long end) implements Comparable<Range> {
 
     @Override
     public int compareTo(Range o) {
-        // Order by lowest start and biggest end
-        int comp = Long.compare(this.start, o.start);
-        if (comp == 0) {
-            comp = Long.compare(o.end, this.end);
-        }
-        return comp;
+        return Long.compare(this.start, o.start);
     }
 }
